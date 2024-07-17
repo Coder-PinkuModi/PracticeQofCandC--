b08e08692535acfccf921f3f32a872fa031af9e3
Coder-PinkuModi <thecoderpinku@gmail.com>
2024-07-17 14:27:24 +0530

    learned stl- vector

--- C++/Lec-27(STL,Vector)/vectorBasic.cpp
+++ C++/Lec-27(STL,Vector)/vectorBasic.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<algorithm>
 using namespace std;
 
 int main()
@@ -7,6 +8,17 @@ int main()
     vector<int> v;
     v.push_back(1);
     v.push_back(4);
-    cout << v[0] << " " << v[1];
+    v.push_back(8);
+    v.push_back(9);
+    for(auto i:v){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+    auto it = find(v.begin(),v.end(),9); // this will be like a pointer or a iterator points into the element to find the index of it follow up the next line
+    if(it!=v.end()){
+         auto index = distance( v.begin(), it); // to find the index
+         cout<<index;
+    }
+    
     return 0;
 }
