c9f05d9b84ba38649bec099f5a5ea453f6f7f7cb
Coder-PinkuModi <thecoderpinku@gmail.com>
2024-09-05 15:10:09 +0530

    fixed the issues in the code

--- C++/Lec-34(2D Array prac)/binarysearchinSortedArray.cpp
+++ C++/Lec-34(2D Array prac)/binarysearchinSortedArray.cpp
@@ -1,23 +1,23 @@
 // there should be given a mtrix which is sorted in decreasing order and a target given, have to return bool value from function if the value is present the matrix
 // leetcode--> in ascending order given in the question of the leetcode
 
-#include<iosstream>
+#include<iostream>
 #include<vector>
 #include<algorithm>
 
 using namespace std;
 
-int findTarget(vector<<vector<int> >matrix,int target){
+bool findTarget(vector<vector<int> >matrix,int target){
     int n=matrix.size(),m=matrix[0].size();
     int left=0,right=(m*n)-1;
     while(left<=right){
-        int mid=left+right/2;
+        int mid = left + (right - left) / 2;
         int row_index=mid/m;
-        int col_index%col;
+        int col_index=mid%col;
 
         if(matrix[row_index][col_index]==target) return 1;
-        else if(matrix[row_index][col_index]>target) right=mid-1;
-        else left=mid+1;
+        else if(matrix[row_index][col_index]>target) left=mid+1;
+        else right=mid-1;
     }
 
     return 0;
@@ -32,7 +32,7 @@ int main(){
     cout<<"Enter the marix elements in descending order: ";
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            cin>>matrix[n][m];
+            cin>>matrix[i][j];
         }
     }
     cout<<"Enter the targer to find in the array: ";
